e54b99e267fb73d56e2fb76f277b73900dae4cf4
Todd Benson <todd.benson@csaa.com>
2017-05-16 16:15:44 -0700

    Changes

--- custom.el
+++ custom.el
@@ -3,13 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
     ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(package-selected-packages
    (quote
-    (yasnippet git-blame whole-line-or-region whitespace-cleanup-mode wgrep-ag vc-darcs unfill undo-tree switch-window smex session scratch project-local-variables page-break-lines mwe-log-commands multiple-cursors move-dup mmm-mode json-mode idomenu ido-ubiquitous ibuffer-vc highlight-symbol highlight-escape-sequences guide-key fullframe flycheck fill-column-indicator expand-region exec-path-from-shell dired-sort dired+ diminish diff-hl default-text-scale darcsum color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized browse-kill-ring avy auto-complete anzu ag)))
+    (git-blame yasnippet whole-line-or-region whitespace-cleanup-mode wgrep-ag vc-darcs unfill undo-tree switch-window smex session scratch project-local-variables page-break-lines org-pomodoro mwe-log-commands multiple-cursors move-dup mmm-mode json-mode idomenu ido-ubiquitous ibuffer-vc highlight-symbol highlight-escape-sequences guide-key git-blamed fullframe flycheck fill-column-indicator expand-region exec-path-from-shell dired-sort dired+ diminish diff-hl default-text-scale darcsum color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized browse-kill-ring avy auto-complete anzu ag)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
